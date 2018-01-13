1a1eeae4b8fd2655eed7d155038496ddfd016d34
Christian Nikolov <christian@newton.codes>
2018-01-13 06:41:19 +0200

    Add process.exit on build and push

--- bin/dev/build.js
+++ bin/dev/build.js
@@ -7,3 +7,5 @@ __dirname = __dirname.replace(/\\/g, '/');
 exec('cd ' + __dirname + '/../../docker && docker build -t newtoncodes/vpn .');
 exec('cd ' + __dirname + '/../../docker && docker build -t newtoncodes/vpn:' + version + ' .');
 
+console.log('Done.');
+process.exit(0);
--- bin/dev/push.js
+++ bin/dev/push.js
@@ -6,3 +6,6 @@ require('./build');
 
 exec('docker push newtoncodes/vpn');
 exec('docker push newtoncodes/vpn:' + version);
+
+console.log('Done.');
+process.exit(0);
